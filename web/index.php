2077b1b37bfe8d09d303ad802bce69c59f9fe148
Brice Favre <brice.favre@blogspirit.com>
2013-01-10 13:50:50 +0100

    Creating New route and init data

--- web/index.php
+++ web/index.php
@@ -4,6 +4,34 @@
 require_once  __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
+
+$app->get('/', function() use ($app) {
+    return 'Welcome to my new website';
+});
+
+$news = array(
+    'news_id' => array('id'=> 'news_id', 'title'=> 'Title of the news')
+);
+
+$app->get('/news', function() use ($news) {
+    $output = '';
+    foreach ($news as $new) {
+        $output .= '<a href="/news/'.$new['id'].'">'.$new['title'].'</a>';
+    }
+    return $output;
+});
+
+$app->get('/news/{id}', function(Silex\Application $app, $id) use ($news) {
+    if (!isset ($news[$id])) {
+        $app->abort(404, 'Post '.$id.' not found');
+    }
+    
+    $new = $news[$id];
+    
+    $output = '<h1>'.$new['title'].'</h1>';
+    return $output;
+});
 
 $app->get('/hello/{name}', function($name) use ($app) {
     return 'Hello '.$app->escape($name);
