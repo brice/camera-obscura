cb43afd37267dfad0ee042c277e98516a3734c28
Brice Favre <brice.favre@blogspirit.com>
2013-01-11 14:09:41 +0100

    Change Readme and add a contact form

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-camera-obscura
+Website camera.obscura.fr
 ==============
+
+This project is a first work with the Silex Framework and will be the part of my website
--- web/index.php
+++ web/index.php
@@ -3,8 +3,12 @@
 
 require_once  __DIR__.'/../vendor/autoload.php';
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 $app = new Silex\Application();
 $app['debug'] = true;
+$config = array('email', 'Your email');
 
 $app->get('/', function() use ($app) {
     return 'Welcome to my new website';
@@ -33,8 +37,19 @@ $app->get('/news/{id}', function(Silex\Application $app, $id) use ($news) {
     return $output;
 });
 
-$app->get('/hello/{name}', function($name) use ($app) {
-    return 'Hello '.$app->escape($name);
+$app->get('/contact', function() use($app) {
+
+    $output = '<form method="post" action="/contact"><label for="message">Send me a message</label><input type="text" name="message" id="message"><input type="submit"></form>';
+    
+    return $output;
+});
+
+
+$app->post('/contact', function(Request $request) use($config) {
+
+    $message = $request->get('message');
+    mail($config['email'], 'Message from your site', $message);
+    return new Response('Thank you for your message!', 201);
 });
 
 $app->run();
